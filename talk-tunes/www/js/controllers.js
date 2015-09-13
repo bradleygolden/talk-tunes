6d566784b06f43624df07a5474ca5b33a89bc0d3
Bradley Golden <bradleygolden@0587337719.wireless.umich.net>
2015-09-12 20:15:47 -0400

    added geolocation json feeds

--- talk-tunes/www/js/controllers.js
+++ talk-tunes/www/js/controllers.js
@@ -1,35 +1,46 @@
 myApp.controller("LoginController", function($scope, $firebaseAuth, $state) {
   var ref = new Firebase("https://talk-tunes.firebaseio.com");
 
+  remove_user = function(authData){
+    //console.log(authData.uid);
+    var userRef = new Firebase("https://talk-tunes.firebaseio.com/users/" + authData.uid);
+    userRef.remove();
+  }
+
+  set_user = function(authData){
+    var userRef = new Firebase("https://talk-tunes.firebaseio.com/users/" + authData.uid);
+    //console.log(authData.uid);
+    userRef.set(authData.uid);
+  };
+
   // Create a callback to handle the result of the authentication
   function authHandler(error, authData) {
     if (error) {
-      console.log("Login Failed!", error);
+      console.error("Login Failed!", error);
     } else {
-      console.log("Authenticated successfully with payload:", authData);
+      //console.log("Authenticated successfully with payload:", authData);
+      set_user(authData);
       $state.go('home');
     }
   }
 
   // Logs a user in
   $scope.login = function(provider){
-      var authData = ref.getAuth();
-      if (authData){
-        $state.go('home');
-      }
-      else{
-        // if user not logged it, force oauth process
-        ref.authWithOAuthPopup(provider, authHandler),{
-          remember: "sessionOnly",
-          scope: "email"
-        };
-      }
-  };
+    var authData = ref.getAuth();
 
-  // Logs a user out
-  $scope.logout = function() {
-    console.log("logout");
-    ref.unauth();
+    // user is logged in already
+    //console.log(authData);
+    if (authData){
+      set_user(authData);
+      $state.go('home');
+    }
+    else{
+      // if user not logged it, force oauth process
+      ref.authWithOAuthPopup(provider, authHandler),{
+        remember: "sessionOnly",
+        scope: "email"
+      };
+    }
   };
 });
 
@@ -38,7 +49,7 @@ myApp.factory('Items', ['$firebaseArray', function($firebaseArray) {
   return $firebaseArray(itemsRef);
 }])
 
-myApp.controller('HomeController', function($scope, $ionicListDelegate, Items, $state) {
+myApp.controller('HomeController', function($scope, $ionicListDelegate, Items, $state, $cordovaGeolocation) {
 
   $scope.items = Items;
 
@@ -59,8 +70,43 @@ myApp.controller('HomeController', function($scope, $ionicListDelegate, Items, $
 
   // Logs a user out
   $scope.logout = function() {
-    var ref = new Firebase('https://talk-tunes.firebaseio.com/login');
-    ref.unauth();
+    var ref = new Firebase("https://talk-tunes.firebaseio.com/");
+    var authData = ref.getAuth(); // get auth object
+    remove_user(authData); // delete user from database
+    ref.unauth(); // sign out of auth
     $state.go('login');
   };
+
+  var update_lat_long = function(authData, lat, long){
+    //console.log(lat, long);
+    var userRef = new Firebase("https://talk-tunes.firebaseio.com/users/" + authData.uid + "/");
+    userRef.update({
+      latitude: lat,
+      longitude: long
+    }, function(error) {
+    if (error) {
+      console.error("Data could not be saved." + error);
+    } else {
+      //console.error("Data saved successfully.");
+  }
+});
+  };
+
+
+  var ref = new Firebase("https://talk-tunes.firebaseio.com/");
+  var authData = ref.getAuth();
+  var posOptions = {timeout: 10000, enableHighAccuracy: true};
+   callback = function(){$cordovaGeolocation
+     .getCurrentPosition(posOptions)
+     .then(function (position) {
+       var lat  = position.coords.latitude
+       var long = position.coords.longitude
+       update_lat_long(authData, lat, long);
+       setTimeout(callback, 5000);
+     }, function(err) {
+       console.error("Couldn't not get location data.");
+     });
+   };
+   callback();
+
 });
